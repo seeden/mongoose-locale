cf9c31d7ea5c6b47ece0b92d6ca2d7be61b1f33d
Zlatko Fedor <zfedor@gmail.com>
2014-09-05 19:07:27 +0200

    first version

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+
+module.exports = function localePlugin (schema, options) {
+	//prepare arguments
+	if(!options) {
+		options = {};
+	}
+
+
+	schema.eachPath(function(path, config) {
+    	if (!config.options.locale) {
+    		return;
+    	}
+
+    	//clean actual path
+    	delete(config.options.locale);
+      	schema.remove(path);
+
+      	var nested = {
+      		lg    : { type: String },
+      		value : config.options
+      	};
+
+      	schema.path(path, nested);
+    });
+};
