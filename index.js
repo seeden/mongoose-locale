829a735cdf5d09a2895cdd5d559bdd3b8b2a9c75
Zlatko Fedor <zfedor@gmail.com>
2014-09-08 18:58:43 +0200

    style

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
 'use strict';
 
-
 module.exports = function localePlugin (schema, options) {
 	//prepare arguments
 	if(!options) {
 		options = {};
 	}
 
-
 	schema.eachPath(function(path, config) {
-    	if (!config.options.locale) {
-    		return;
-    	}
+		if (!config.options.locale) {
+			return;
+		}
 
-    	//clean actual path
-    	delete(config.options.locale);
-      	schema.remove(path);
+		//clean actual path
+		delete(config.options.locale);
+		schema.remove(path);
 
-      	var nested = {
-      		lg    : { type: String },
-      		value : config.options
-      	};
+		var nested = {
+			lg    : { type: String },
+			value : config.options
+		};
 
-      	schema.path(path, nested);
-    });
+		schema.path(path, nested);
+	});
 };
