1c546d2d0e85a06c9941194652db9b8ca3786bad
Zlatko Fedor <zfedor@gmail.com>
2014-10-06 10:16:22 +1300

    added localised methods

--- index.js
+++ index.js
@@ -33,4 +33,46 @@ module.exports = function localePlugin (schema, options) {
 		//replace path
 		schema.path(path, nested);
 	});
+
+	schema.methods.getPropertyLocalised = function(property, locale, defaultValue) {
+		defaultValue = defaultValue || null;
+
+		var prop = this.get(property);
+		if(!prop || !prop.length) {
+			return defaultValue;
+		}
+
+		for(var i=0; i<prop.length; i++) {
+			if(prop[i].lg === locale) {
+				return prop[i].value;
+			}
+		}
+
+		return defaultValue;		
+	};
+
+	schema.methods.hasPropertyLocale = function(property, locale) {
+		var locales = this.getPropertyLocales(property);
+
+		if(locales.indexOf(locale)!==-1) {
+			return true;
+		}
+
+		return false;
+	};
+
+	schema.methods.getPropertyLocales = function(property) {
+		var prop = this.get(property),
+			locales = [];
+
+		if(!prop || !prop.length) {
+			return locales;
+		}
+
+		for(var i=0; i<prop.length; i++) {
+			locales.push(prop[i].lg);
+		}
+
+		return locales;
+	};
 };
